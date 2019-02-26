fee601f33916c6ad67f4517d03d728222d6fb179
Javier Carceller <elxavicio@gmail.com>
2019-02-26 10:55:31 +0800

    Adds Head component with title

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ import {
   Visibility,
   Step
 } from "semantic-ui-react";
+import Head from "next/head";
 
 // Heads up!
 // We using React Static to prerender our docs with server side rendering, this is a quite simple solution.
@@ -218,159 +219,173 @@ ResponsiveContainer.propTypes = {
 };
 
 const HomepageLayout = () => (
-  <ResponsiveContainer>
-    <Segment style={{ padding: "4em 0em" }} vertical>
-      <Grid container stackable verticalAlign="middle">
-        <Grid.Row>
-          <Grid.Column textAlign="center">
-            <Step.Group size="huge" stackable="mobile" widths={6}>
-              <Step>
-                <Icon name="check" />
-                <Step.Content>
-                  <Step.Title style={{ fontSize: "1em", marginBottom: "1em" }}>
-                    Enable the app
-                  </Step.Title>
-                  <Step.Description
-                    style={{ fontSize: "0.8em", marginBottom: "1em" }}
-                  >
-                    Activate the app when you have to leave your laptop in a
-                    public place (like going to the toilet when working in a
-                    coffee shop).
-                  </Step.Description>
-                </Step.Content>
-              </Step>
-              <Step>
-                <Icon name="volume up" />
-                <Step.Content>
-                  <Step.Title style={{ fontSize: "1em", marginBottom: "1em" }}>
-                    Alarma Is On
-                  </Step.Title>
-                  <Step.Description
-                    style={{ fontSize: "0.8em", marginBottom: "1em" }}
-                  >
-                    If your laptop gets unplugged or the lid closes your laptop
-                    will set up its volume to 200% and the alarm will play.
-                  </Step.Description>
-                </Step.Content>
-              </Step>
-              <Step>
-                <Icon name="coffee" />
-                <Step.Content>
-                  <Step.Title style={{ fontSize: "1em", marginBottom: "1em" }}>
-                    Do what you have to do
-                  </Step.Title>
-                  <Step.Description
-                    style={{ fontSize: "0.8em", marginBottom: "1em" }}
-                  >
-                    Now you can safely go and order that Decaf, Soy Latte With
-                    An Extra Shot And Cream or go and do your business.
-                  </Step.Description>
-                </Step.Content>
-              </Step>
-            </Step.Group>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-
-    <Segment style={{ padding: "0em" }} vertical>
-      <Grid celled="internally" columns="equal" stackable>
-        <Grid.Row textAlign="center">
-          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              "What a Company"
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              That is what they all say about us
-            </p>
-          </Grid.Column>
-          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              "I shouldn't have gone with their competitor."
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              <Image avatar src="/images/avatar/large/nan.jpg" />
-              <b>Nan</b> Chief Fun Officer Acme Toys
-            </p>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-
-    <Segment style={{ padding: "8em 0em" }} vertical>
-      <Container text>
-        <Header as="h3" style={{ fontSize: "2em" }}>
-          Breaking The Grid, Grabs Your Attention
-        </Header>
-        <p style={{ fontSize: "1.33em" }}>
-          Instead of focusing on content creation and hard work, we have learned
-          how to master the art of doing nothing by providing massive amounts of
-          whitespace and generic content that can seem massive, monolithic and
-          worth your attention.
-        </p>
-        <Button as="a" size="large">
-          Read More
-        </Button>
-
-        <Divider
-          as="h4"
-          className="header"
-          horizontal
-          style={{ margin: "3em 0em", textTransform: "uppercase" }}
-        >
-          <a href="#">Case Studies</a>
-        </Divider>
-
-        <Header as="h3" style={{ fontSize: "2em" }}>
-          Did We Tell You About Our Bananas?
-        </Header>
-        <p style={{ fontSize: "1.33em" }}>
-          Yes I know you probably disregarded the earlier boasts as non-sequitur
-          filler content, but it's really true. It took years of gene splicing
-          and combinatory DNA research, but our bananas can really dance.
-        </p>
-        <Button as="a" size="large">
-          I'm Still Quite Interested
-        </Button>
-      </Container>
-    </Segment>
-
-    <Segment inverted vertical style={{ padding: "5em 0em" }}>
-      <Container>
-        <Grid divided inverted stackable>
+  <div>
+    <Head>
+      <title>Alarma App - Antitheft app for laptops</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <ResponsiveContainer>
+      <Segment style={{ padding: "4em 0em" }} vertical>
+        <Grid container stackable verticalAlign="middle">
           <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="About" />
-              <List link inverted>
-                <List.Item as="a">Sitemap</List.Item>
-                <List.Item as="a">Contact Us</List.Item>
-                <List.Item as="a">Religious Ceremonies</List.Item>
-                <List.Item as="a">Gazebo Plans</List.Item>
-              </List>
+            <Grid.Column textAlign="center">
+              <Step.Group size="huge" stackable="mobile" widths={6}>
+                <Step>
+                  <Icon name="check" />
+                  <Step.Content>
+                    <Step.Title
+                      style={{ fontSize: "1em", marginBottom: "1em" }}
+                    >
+                      Enable the app
+                    </Step.Title>
+                    <Step.Description
+                      style={{ fontSize: "0.8em", marginBottom: "1em" }}
+                    >
+                      Activate the app when you have to leave your laptop in a
+                      public place (like going to the toilet when working in a
+                      coffee shop).
+                    </Step.Description>
+                  </Step.Content>
+                </Step>
+                <Step>
+                  <Icon name="volume up" />
+                  <Step.Content>
+                    <Step.Title
+                      style={{ fontSize: "1em", marginBottom: "1em" }}
+                    >
+                      Alarma Is On
+                    </Step.Title>
+                    <Step.Description
+                      style={{ fontSize: "0.8em", marginBottom: "1em" }}
+                    >
+                      If your laptop gets unplugged or the lid closes your
+                      laptop will set up its volume to 200% and the alarm will
+                      play.
+                    </Step.Description>
+                  </Step.Content>
+                </Step>
+                <Step>
+                  <Icon name="coffee" />
+                  <Step.Content>
+                    <Step.Title
+                      style={{ fontSize: "1em", marginBottom: "1em" }}
+                    >
+                      Do what you have to do
+                    </Step.Title>
+                    <Step.Description
+                      style={{ fontSize: "0.8em", marginBottom: "1em" }}
+                    >
+                      Now you can safely go and order that Decaf, Soy Latte With
+                      An Extra Shot And Cream or go and do your business.
+                    </Step.Description>
+                  </Step.Content>
+                </Step>
+              </Step.Group>
             </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="Services" />
-              <List link inverted>
-                <List.Item as="a">Banana Pre-Order</List.Item>
-                <List.Item as="a">DNA FAQ</List.Item>
-                <List.Item as="a">How To Access</List.Item>
-                <List.Item as="a">Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as="h4" inverted>
-                Footer Header
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Segment style={{ padding: "0em" }} vertical>
+        <Grid celled="internally" columns="equal" stackable>
+          <Grid.Row textAlign="center">
+            <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
+              <Header as="h3" style={{ fontSize: "2em" }}>
+                "What a Company"
               </Header>
-              <p>
-                Extra space for a call to action inside the footer that could
-                help re-engage users.
+              <p style={{ fontSize: "1.33em" }}>
+                That is what they all say about us
+              </p>
+            </Grid.Column>
+            <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
+              <Header as="h3" style={{ fontSize: "2em" }}>
+                "I shouldn't have gone with their competitor."
+              </Header>
+              <p style={{ fontSize: "1.33em" }}>
+                <Image avatar src="/images/avatar/large/nan.jpg" />
+                <b>Nan</b> Chief Fun Officer Acme Toys
               </p>
             </Grid.Column>
           </Grid.Row>
         </Grid>
-      </Container>
-    </Segment>
-  </ResponsiveContainer>
+      </Segment>
+
+      <Segment style={{ padding: "8em 0em" }} vertical>
+        <Container text>
+          <Header as="h3" style={{ fontSize: "2em" }}>
+            Breaking The Grid, Grabs Your Attention
+          </Header>
+          <p style={{ fontSize: "1.33em" }}>
+            Instead of focusing on content creation and hard work, we have
+            learned how to master the art of doing nothing by providing massive
+            amounts of whitespace and generic content that can seem massive,
+            monolithic and worth your attention.
+          </p>
+          <Button as="a" size="large">
+            Read More
+          </Button>
+
+          <Divider
+            as="h4"
+            className="header"
+            horizontal
+            style={{ margin: "3em 0em", textTransform: "uppercase" }}
+          >
+            <a href="#">Case Studies</a>
+          </Divider>
+
+          <Header as="h3" style={{ fontSize: "2em" }}>
+            Did We Tell You About Our Bananas?
+          </Header>
+          <p style={{ fontSize: "1.33em" }}>
+            Yes I know you probably disregarded the earlier boasts as
+            non-sequitur filler content, but it's really true. It took years of
+            gene splicing and combinatory DNA research, but our bananas can
+            really dance.
+          </p>
+          <Button as="a" size="large">
+            I'm Still Quite Interested
+          </Button>
+        </Container>
+      </Segment>
+
+      <Segment inverted vertical style={{ padding: "5em 0em" }}>
+        <Container>
+          <Grid divided inverted stackable>
+            <Grid.Row>
+              <Grid.Column width={3}>
+                <Header inverted as="h4" content="About" />
+                <List link inverted>
+                  <List.Item as="a">Sitemap</List.Item>
+                  <List.Item as="a">Contact Us</List.Item>
+                  <List.Item as="a">Religious Ceremonies</List.Item>
+                  <List.Item as="a">Gazebo Plans</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as="h4" content="Services" />
+                <List link inverted>
+                  <List.Item as="a">Banana Pre-Order</List.Item>
+                  <List.Item as="a">DNA FAQ</List.Item>
+                  <List.Item as="a">How To Access</List.Item>
+                  <List.Item as="a">Favorite X-Men</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={7}>
+                <Header as="h4" inverted>
+                  Footer Header
+                </Header>
+                <p>
+                  Extra space for a call to action inside the footer that could
+                  help re-engage users.
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Segment>
+    </ResponsiveContainer>
+  </div>
 );
 
 export default HomepageLayout;
