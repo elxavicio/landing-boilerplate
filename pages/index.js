57716d5cf08e7e4765c90a960fee2d79958f29b8
Javier Carceller <elxavicio@gmail.com>
2019-02-27 15:07:31 +0800

    Gets footer done

--- pages/index.js
+++ pages/index.js
@@ -213,40 +213,30 @@ const HomepageLayout = () => (
         </Grid>
       </Segment>
 
-      <Segment inverted vertical style={{ padding: "5em 0em" }}>
-        <Container>
-          <Grid divided inverted stackable>
-            <Grid.Row>
-              <Grid.Column width={3}>
-                <Header inverted as="h4" content="About" />
-                <List link inverted>
-                  <List.Item as="a">Sitemap</List.Item>
-                  <List.Item as="a">Contact Us</List.Item>
-                  <List.Item as="a">Religious Ceremonies</List.Item>
-                  <List.Item as="a">Gazebo Plans</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={3}>
-                <Header inverted as="h4" content="Services" />
-                <List link inverted>
-                  <List.Item as="a">Banana Pre-Order</List.Item>
-                  <List.Item as="a">DNA FAQ</List.Item>
-                  <List.Item as="a">How To Access</List.Item>
-                  <List.Item as="a">Favorite X-Men</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={7}>
-                <Header as="h4" inverted>
-                  Footer Header
-                </Header>
-                <p>
-                  Extra space for a call to action inside the footer that could
-                  help re-engage users.
-                </p>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
+      <Segment
+        inverted
+        vertical
+        style={{ padding: "5em 0em" }}
+        textAlign="center"
+      >
+        <p>
+          Made with love by{" "}
+          <a href="https://javiercarceller.com"> Javier Carceller </a>
+        </p>
+
+        <p>
+          <Icon size="big" name="github" link="https://github.com/elxavicio" />{" "}
+          <Icon
+            size="big"
+            name="linkedin"
+            link="https://www.linkedin.com/in/javiercarceller/"
+          />
+          <Icon
+            size="big"
+            name="twitter"
+            link="https://twitter.com/javiercarceller"
+          />
+        </p>
       </Segment>
     </ResponsiveContainer>
   </div>
